11fb5f0799aa15d4f641ac9850f221ef17204148
Sasha Chernik <alexandra.chernik@gmail.com>
2020-11-06 12:04:53 -0500

    Update model.py

--- model.py
+++ model.py
@@ -3,13 +3,15 @@ import nltk
 import csv
 
 def get_word_list(text) :
+    #print(text)
     word_list = []
     word_list.extend(nltk.word_tokenize(text.lower()))
+    #print(word_list)
 
     #Removed some punctuation (there is a more sophisticated way to do this! Need a tokenizer which will remove punctuation and common words)
-    vocab = [i for i in vocab if (i != "!" and i != "," and i != "." and i != "(" and i != ")" and i != ";" and i != ":")]
+    word_list = [i for i in word_list if (i != "!" and i != "," and i != "." and i != "(" and i != ")" and i != ";" and i != ":")]
     
-    return world_list
+    return word_list
 
 def create_dictionary(abstracts) :
     dictionary = dict()
@@ -20,11 +22,13 @@ def create_dictionary(abstracts) :
             if word not in dictionary :
                 dictionary[word] = i
                 i += 1
+    #print(dictionary)
     return dictionary
 
+# consider redoing dictionary as a numpy array?
 def create_matrix(abstracts, dictionary) :
     n = len(abstracts)
-    m = len(word_dictionary)
+    m = len(dictionary)
 
     matrix = np.zeros((n,m), dtype=int)
     for i in range(n) :
@@ -35,20 +39,18 @@ def create_matrix(abstracts, dictionary) :
     return matrix
 
 
-
-
 def fit_naive_bayes(train_matrix, train_labels) :
     num_train_abstracts, size_dictionary = train_matrix.shape
     # _, num_labels = train_labels.shape # This is irrelevant right now, as we are working with COMPUTER SCIENCE labels only
-    log_phi_y1 = (np.sum(labels)+1)/(len(labels)+2)
-    log_phi_y0 = np.log(1 - phi_y_1)
-    log_phi_y1 = np.log(phi_y_1)
+    log_phi_y1 = (np.sum(train_labels)+1)/(len(train_labels)+2)
+    log_phi_y0 = np.log(1 - log_phi_y1)
+    log_phi_y1 = np.log(log_phi_y1)
 
     denom = 0
     for i in range(num_train_abstracts) :
         if train_labels[i] == 1 :
             denom += np.sum(train_matrix[i])
-    laplace = 1/(denom + m)
+    laplace = 1/(denom + size_dictionary)
 
     log_phi1 = np.log(np.full(size_dictionary, laplace))
 
@@ -60,16 +62,16 @@ def fit_naive_bayes(train_matrix, train_labels) :
         log_phi1[k] = np.log((val+1)/(denom + size_dictionary))
 
     denom = 0
-    for i in range(n) :
+    for i in range(num_train_abstracts) :
         if train_labels[i] == 0 :
             denom += np.sum(train_matrix[i])
     laplace = 1/(denom + size_dictionary)
 
-    phi_0 = np.log(np.full(size_dictionary, laplace))
+    log_phi0 = np.log(np.full(size_dictionary, laplace))
 
     for k in range(size_dictionary) :
         val = 0
-        for i in range(n) :
+        for i in range(num_train_abstracts) :
             if train_labels[i] == 0 :
                 val += train_matrix[i][k]
         log_phi0[k] = np.log((val+1)/(denom + size_dictionary))
@@ -80,8 +82,10 @@ def predict_naive_bayes(model, test_matrix) :
     log_phi_y1, log_phi_y0, log_phi1, log_phi0 = model
 
     num_test_abstracts, size_dictionary = test_matrix.shape
-    
-    pred = np.zeros(num_test_abstracts) # This will need to be multi-dimensional as well, for the different values of y!
+    #print('num test abstracts: ', num_test_abstracts)
+    #print('test matrix shape: ', test_matrix.shape)
+
+    pred = np.zeros(num_test_abstracts, dtype=int) # This will need to be multi-dimensional as well, for the different values of y!
     for i in range(num_test_abstracts) :
         log_p1 = log_phi_y1
         log_p0 = log_phi_y0
@@ -100,7 +104,7 @@ def get_labels(dataset_input_path) :
     with open(dataset_input_path) as dataset :
         reader = csv.reader(dataset, delimiter=',')
         for row in reader :
-            labels.extend(f'{row[3]}')
+            labels.append(int((f'{row[3]}')))
     return labels
 
 
@@ -109,17 +113,18 @@ def pre_process_train(train_input_path, dictionary_path, train_matrix_path) :
     with open(train_input_path) as train_file :
         reader = csv.reader(train_file, delimiter=',')
         for row in reader :
-            abstracts.extend(f'{row[2]}')
+            abstracts.append(f'{row[2]}')
 
     dictionary = create_dictionary(abstracts)
     matrix = create_matrix(abstracts, dictionary)
     labels = get_labels(train_input_path)
 
-    with open(dictionary_path,'w') as f :
+    """
+    with open(dictionary_path,'w') as f : # This is not currently saving the indexes
         f.write(','.join(dictionary))
 
-    with open(train_matrix_path, 'w') as f :
-        f.write(','.join(matrix))
+    #np.savetxt(train_matrix_path, matrix)
+    """    
 
     return matrix, dictionary, labels
 
@@ -128,8 +133,30 @@ def pre_process_test(test_input_path, dictionary) :
     with open(test_input_path) as test_file :
         reader = csv.reader(test_file, delimiter=',')
         for row in reader :
-            abstracts.extend(f'{row[2]}')
+            abstracts.append(f'{row[2]}')
     matrix = create_matrix(abstracts, dictionary)
     labels = get_labels(test_input_path)
 
     return matrix, labels
+
+def main() :
+    train_input_path = 'dataset/train_first_three_no_header.csv'
+    dictionary_path = 'data_processing/dictionary.txt'
+    train_matrix_path = 'data_processing/train_matrix.txt'
+    test_input_path = 'dataset/test_three_no_header.csv'
+
+    train_matrix, dictionary, train_labels = pre_process_train(train_input_path, dictionary_path, train_matrix_path)
+
+    #print('train labels: ', train_labels)
+
+    nb_model = fit_naive_bayes(train_matrix, train_labels)
+
+    test_matrix, test_labels = pre_process_test(test_input_path, dictionary)
+
+    pred_labels = predict_naive_bayes(nb_model, test_matrix)
+    #print(len(pred_labels))
+    print('pred_labels: ', pred_labels)
+
+
+if __name__ == "__main__":
+    main()
