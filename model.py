71f43632edcc434c7c7f00e2e6cbc37f4b40e079
Sasha Chernik <alexandra.chernik@gmail.com>
2020-11-27 14:03:20 -0500

    Add files via upload

--- model.py
+++ model.py
@@ -1,174 +1,242 @@
 import numpy as np
 import nltk
+#nltk.download('stopwords')
 import csv
+import string
+import re
+import time
 
-def get_word_list(text) :
+def create_stopwords() :
+
+    stopwords = nltk.corpus.stopwords.words('english')
+    stopwords.extend(list(string.punctuation))
+
+    return stopwords
+
+def get_word_list(text, stopwords) :
     #print(text)
-    word_list = []
-    word_list.extend(nltk.word_tokenize(text.lower()))
-    #print(word_list)
+    word_list = [w.lower() for w in nltk.word_tokenize(text)]
+    #word_list.extend(nltk.word_tokenize(text.lower()))
+    #print(word_list) 
 
-    #Removed some punctuation (there is a more sophisticated way to do this! Need a tokenizer which will remove punctuation and common words)
-    word_list = [i for i in word_list if (i != "!" and i != "," and i != "." and i != "(" and i != ")" and i != ";" and i != ":")]
+    word_list = [word for word in word_list if not word in stopwords]
+    word_list = [word for word in word_list if (re.search("'", word) == None)]
+    # find a way to take this ^ out of this function? For cleanliness
+    # also consider changing, what if there are significant contractions which are meaningful?
     
     return word_list
 
-def create_dictionary(abstracts) :
+def create_dictionary(abstracts, stopwords) :
+
+    print("creating dictionary")
+
     dictionary = dict()
-    i = 0
     for abstract in abstracts :
-        abstract_list = get_word_list(abstract)
-        for word in abstract_list :
-            if word not in dictionary :
-                dictionary[word] = i
-                i += 1
+        abstract_arr = np.unique(get_word_list(abstract, stopwords))
+        for word in abstract_arr :
+            if word in dictionary :
+                dictionary[word] += 1
+            else :
+                dictionary[word] = 1
+
+    print("    dictionary size before removing rare words: ", len(dictionary))
+
+    dictionary = {word:count for word, count in dictionary.items() if count >= 5}
+
+    print("    dictionary size after removing rare words: ", len(dictionary))
+
+    i = 0
+    for word in dictionary :
+        dictionary[word] = i
+        i+=1
     #print(dictionary)
     return dictionary
 
 # consider redoing dictionary as a numpy array?
-def create_matrix(abstracts, dictionary) :
+def create_matrix(abstracts, dictionary, stopwords) :
     n = len(abstracts)
     m = len(dictionary)
 
     matrix = np.zeros((n,m), dtype=int)
     for i in range(n) :
-        words = get_word_list(abstracts[i])
+        words = get_word_list(abstracts[i], stopwords)
         for word in words :
             if word in dictionary:
                 matrix[i, dictionary[word]] += 1
     return matrix
 
+def pre_process_train(train_data_path, dictionary_path, train_matrix_path, stopwords) :
 
-def fit_naive_bayes(train_matrix, train_labels) :
-    num_train_abstracts, size_dictionary = train_matrix.shape
-    # _, num_labels = train_labels.shape # This is irrelevant right now, as we are working with COMPUTER SCIENCE labels only
-    log_phi_y1 = (np.sum(train_labels)+1)/(len(train_labels)+2)
+    abstracts = []
+    with open(train_data_path) as train_data_file :
+        reader = csv.reader(train_data_file, delimiter=',')
+        for row in reader :
+            abstracts.append(f'{row[1]}')
+
+    dictionary = create_dictionary(abstracts, stopwords)
+    matrix = create_matrix(abstracts, dictionary, stopwords)
+
+    with open(dictionary_path, 'w') as dict_file :
+        writer = csv.writer(dict_file, delimiter=',')
+        for word, index in dictionary.items() :
+            writer.writerow([word, index])
+    np.savetxt(train_matrix_path, matrix, fmt='%i')
+
+    return dictionary, matrix
+
+def fit_naive_bayes_vectorized(train_matrix, train_labels) :
+    # train_matrix shape = number of abstracts, number of words
+    # train labels size = number of abstracts
+    num_abstracts, num_words = train_matrix.shape
+
+    log_phi_y1 = (np.sum(train_labels)+1)/(num_abstracts+2)
     log_phi_y0 = np.log(1 - log_phi_y1)
     log_phi_y1 = np.log(log_phi_y1)
 
-    denom = 0
-    for i in range(num_train_abstracts) :
-        if train_labels[i] == 1 :
-            denom += np.sum(train_matrix[i])
-    laplace = 1/(denom + size_dictionary)
+    denom = np.sum(np.sum(train_matrix, axis=1)*train_labels) + num_words
+    log_phi1 = np.log((np.sum((train_matrix.T * train_labels), axis=1)+1)/denom)
 
-    log_phi1 = np.log(np.full(size_dictionary, laplace))
-
-    for k in range(size_dictionary) :
-        val = 0
-        for i in range(num_train_abstracts) :
-            if train_labels[i] == 1 :
-                val += train_matrix[i][k]
-        log_phi1[k] = np.log((val+1)/(denom + size_dictionary))
-
-    denom = 0
-    for i in range(num_train_abstracts) :
-        if train_labels[i] == 0 :
-            denom += np.sum(train_matrix[i])
-    laplace = 1/(denom + size_dictionary)
-
-    log_phi0 = np.log(np.full(size_dictionary, laplace))
-
-    for k in range(size_dictionary) :
-        val = 0
-        for i in range(num_train_abstracts) :
-            if train_labels[i] == 0 :
-                val += train_matrix[i][k]
-        log_phi0[k] = np.log((val+1)/(denom + size_dictionary))
+    denom = np.sum(np.sum(train_matrix, axis=1)*(1-train_labels)) + num_words
+    log_phi0 = np.log((np.sum((train_matrix.T * (1-train_labels)), axis=1)+1)/denom)
 
     return log_phi_y1, log_phi_y0, log_phi1, log_phi0
 
-def predict_naive_bayes(model, test_matrix) :
+def predict_naive_bayes_vectorized(model, test_matrix) :
     log_phi_y1, log_phi_y0, log_phi1, log_phi0 = model
+    num_abstracts, num_words = test_matrix.shape
 
-    num_test_abstracts, size_dictionary = test_matrix.shape
-    #print('num test abstracts: ', num_test_abstracts)
-    #print('test matrix shape: ', test_matrix.shape)
+    log_p1 = np.sum((test_matrix*log_phi1), axis=1) + log_phi_y1
+    log_p0 = np.sum((test_matrix*log_phi0), axis=1) + log_phi_y0
 
-    pred = np.zeros(num_test_abstracts, dtype=int) # This will need to be multi-dimensional as well, for the different values of y!
-    for i in range(num_test_abstracts) :
-        log_p1 = log_phi_y1
-        log_p0 = log_phi_y0
-        for k in range(size_dictionary) :
-            log_p1 += log_phi1[k] * test_matrix[i][k]
-            log_p0 += log_phi0[k] * test_matrix[i][k]
-        if log_p1 > log_p0 :
-            pred[i] = 1
+    pred = np.array(log_p1 > log_p0, dtype=int)
 
     return pred
 
-def get_labels(dataset_input_path, label_row) :
+def get_labels(labels_path, label_row) :
     labels = []
-    with open(dataset_input_path) as dataset :
-        reader = csv.reader(dataset, delimiter=',')
+    with open(labels_path) as labels_file :
+        reader = csv.reader(labels_file, delimiter=',')
         for row in reader :
             labels.append(int((f'{row[label_row]}')))
-    return labels
+    return np.asarray(labels, dtype=int)
 
 
-def pre_process_train(train_input_path) :
+def pre_process_test(test_data_path, test_matrix_path, dictionary, stopwords) :
     abstracts = []
-    with open(train_input_path) as train_file :
-        reader = csv.reader(train_file, delimiter=',')
+    with open(test_data_path) as test_data_file :
+        reader = csv.reader(test_data_file, delimiter=',')
         for row in reader :
-            abstracts.append(f'{row[2]}')
-
-    dictionary = create_dictionary(abstracts)
-    matrix = create_matrix(abstracts, dictionary)
-    #labels = get_labels(train_input_path)
-
-    """
-    with open(dictionary_path,'w') as f : # This is not currently saving the indexes
-        f.write(','.join(dictionary))
-
-    #np.savetxt(train_matrix_path, matrix)
-    """    
-
-    return matrix, dictionary
-
-def pre_process_test(test_input_path, dictionary) :
-    abstracts = []
-    with open(test_input_path) as test_file :
-        reader = csv.reader(test_file, delimiter=',')
-        for row in reader :
-            abstracts.append(f'{row[2]}')
-    matrix = create_matrix(abstracts, dictionary)
-    #labels = get_labels(test_input_path)
+            abstracts.append(f'{row[1]}')
+    matrix = create_matrix(abstracts, dictionary, stopwords)
+    np.savetxt(test_matrix_path, matrix, fmt='%i')
 
     return matrix
 
-def main() :
-    train_input_path = 'dataset/train_no_header.csv'
-    #dictionary_path = 'data_processing/dictionary.txt'
-    #train_matrix_path = 'data_processing/train_matrix.txt'
-    test_input_path = 'dataset/test_no_header.csv'
+def create_conf_matrix(test_labels, pred_labels) :
+
+    TP = 0
+    TN = 0
+    FP = 0
+    FN = 0
+
+    for i in range (len(test_labels)) :
+        if test_labels[i] == 0 :
+            if pred_labels[i] == 0 :
+                TN +=1
+            else :
+                FP +=1
+        else :
+            if pred_labels[i] == 0 :
+                FN +=1
+            else :
+                TP +=1
+
+    return TP, TN, FP, FN
+
+def print_conf_matrix(confusion_matrix, output_path) :
+
+    TP, TN, FP, FN = confusion_matrix
+
+    print("    Number of True Positives: ", TP)
+    print("    Number of True Negatives: ", TN)
+    print("    Number of False Positives: ", FP)
+    print("    Number of False Negatives ", FN)
+
+    if TP == 0 or TN == 0 or FP == 0 or FN == 0 :
+        print("    One or more of the values are equal to 0!")
+        return
+
+    accuracy = round(100 * (TP+TN)/(TP+TN+FP+FN), 2)
+    MR = round(100 * (FP+FN)/(TP+TN+FP+FN), 2)
+    TPR = round(100 * TP/(TP+FN), 2)
+    FPR = round(100 * FP/(TN+FP), 2)
+    TNR = round(100 * TN/(TN+FP), 2)
+    FNR = round(100 * FN/(TP+FN), 2)
+    precisionP = round(100 * TP/(TP+FP), 2)
+    precisionN = round(100 * TN/(TN+FN), 2)
+    prevalenceP = round(100 * (FN+TP)/(TP+TN+FP+FN), 2)
+    prevalenceN = round(100 * (TN+FP)/(TP+TN+FP+FN), 2)
+
+        
+    print("    Overall Accuracy: ", accuracy)
+    print("    Misclassification Rate: ", MR)
+    print("    True positive rate: ", TPR)
+    print("    False positive rate: ", FPR)
+    print("    True negative rate: ", TNR)
+    print("    False negative rate: ", FNR)
+    print("    Precision for positives: ", precisionP)
+    print("    Precision for negatives: ", precisionN)
+    print("    Prevalence of positives: ", prevalenceP)
+    print("    Prevalence of negatives: ", prevalenceN)
+
+
+def make_path(category, item) :
+    return "".join(['output/', item, '_', category, '.txt'])
+
+def test_main() :
+
+    train_data_path = 'dataset/train_data.csv'
+    train_label_path = 'dataset/train_labels.csv'
+    test_data_path = 'dataset/test_data.csv'
+    test_label_path = 'dataset/test_labels.csv'
+
+    dictionary_path = 'output/dictionary.csv'
+    train_matrix_path = 'output/train_matrix.txt'
+    test_matrix_path = 'output/test_matrix.txt'
 
     label_row_dict = {
-        'Computer Science' : 3,
-        'Physics' : 4,
-        'Mathematics' : 5,
-        'Statistics' : 6,
-        'Quantitative Biology' : 7,
-        'Quantitative Finanace' : 8
+        "Computer_Science" : 0,
+        "Physics" : 1,
+        "Mathematics" : 2,
+        "Statistics" : 3,
+        "Quantitative_Biology" : 4,
+        "Quantitative_Finance" : 5
     }
 
-    train_matrix, dictionary = pre_process_train(train_input_path)
-    test_matrix = pre_process_test(test_input_path, dictionary)
+    timer_start = time.perf_counter()
+
+    stopwords = create_stopwords()
+    dictionary, train_matrix = pre_process_train(train_data_path, dictionary_path, train_matrix_path, stopwords)
+    test_matrix = pre_process_test(test_data_path, test_matrix_path, dictionary, stopwords)
+
+    print("Time for preprocessing: ", round(time.perf_counter()-timer_start, 2), " seconds")
 
     for category in label_row_dict :
-        train_labels = get_labels(train_input_path, label_row_dict[category])
-        nb_model = fit_naive_bayes(train_matrix, train_labels)
+        train_labels = get_labels(train_label_path, label_row_dict[category])
 
-        test_labels = get_labels(test_input_path, label_row_dict[category])
-        print('test labels for', category, ":", test_labels)
-        pred_labels = predict_naive_bayes(nb_model, test_matrix)
-        print('predicted labels for', category, ":", pred_labels)
+        nb_model = fit_naive_bayes_vectorized(train_matrix, train_labels)
+
+        test_labels = get_labels(test_label_path, label_row_dict[category])
+        
+        pred_labels = predict_naive_bayes_vectorized(nb_model, test_matrix)
    
+        np.savetxt(make_path(category, "pred_labels"), pred_labels, fmt='%i')
+
         accuracy = np.mean(test_labels == pred_labels) * 100
         print('Naive Bayes accuracy for', category, ":", accuracy, "%")
-    #print(len(pred_labels))
-    #print('pred_labels: ', pred_labels)
+
+        print_conf_matrix(create_conf_matrix(test_labels, pred_labels), make_path(category, "conf_matrix"))
 
 
 if __name__ == "__main__":
-    main()
+    test_main()
---
+++ splitdataset.py
@@ -0,0 +1,97 @@
+import csv
+
+def split_dataset(orig_path, train_path, test_path, header_path, split) :
+    print('Running split_dataset for: ', orig_path)
+
+    line_count = 0
+    train_rows = 0
+    test_rows = 0
+    with open(orig_path) as orig_file, open(train_path, mode='w') as train_file, open(test_path, mode='w') as test_file :
+        reader = csv.reader(orig_file, delimiter=',')
+        train_writer = csv.writer(train_file, delimiter=',')
+        test_writer = csv.writer(test_file, delimiter=',')                    
+        for row in reader :
+            if line_count == 0 :
+                with open(header_path, mode='w') as header_file :                        
+                    header_writer = csv.writer(header_file, delimiter=',')
+                    header_writer.writerow(row)
+            else :
+                if int(row[0]) <= split :
+                    train_writer.writerow(row)
+                    train_rows +=1
+                else :
+                    test_writer.writerow(row)
+                    test_rows+=1
+            line_count +=1
+
+    print('    line_count was ', line_count)
+    print('    train_rows was ', train_rows)
+    print('    test_rows was ', test_rows)
+
+def split_labels(input_path, output_data, output_labels) :
+
+    print('Running split_labels for file: ', input_path)
+
+    rows_written = 0
+    with open(input_path) as input_file :
+        reader = csv.reader(input_file, delimiter=',')
+        with open(output_data, mode='w') as data_file :
+            data_writer = csv.writer(data_file, delimiter=',')
+            with open (output_labels, mode='w') as label_file :
+                label_writer = csv.writer(label_file, delimiter=',')
+                for row in reader :
+                    data_writer.writerow([row[1],row[2]])
+                    label_writer.writerow([row[3],row[4],row[5],row[6],row[7],row[8]])
+                    rows_written +=1
+
+    print('    Number of rows written: ', rows_written)
+
+def make_tiny_dataset(orig_path, tiny_path, size) :
+
+    print('Running make_tiny_dataset')
+
+    line_count = 0
+    with open(orig_path) as orig_file, open(tiny_path, mode='w') as tiny_file :
+        reader = csv.reader(orig_file, delimiter=',')
+        tiny_writer = csv.writer(tiny_file, delimiter=',')                   
+        for row in reader :
+            if line_count <= size :
+                tiny_writer.writerow(row)
+                line_count +=1
+            else :
+                break
+    print('    Number of lines written (includes header): ', line_count)
+
+def main() :
+    orig_path = 'dataset/original_data.csv'
+    train_path = 'dataset/train.csv'
+    test_path = 'dataset/test.csv'
+    header_path = 'dataset/header.csv'
+    train_data_path = 'dataset/train_data.csv'
+    train_labels_path = 'dataset/train_labels.csv'
+    test_data_path = 'dataset/test_data.csv'
+    test_labels_path = 'dataset/test_labels.csv'
+    split = 14681
+
+    split_dataset(orig_path, train_path, test_path, header_path, split)
+    split_labels(train_path, train_data_path, train_labels_path)
+    split_labels(test_path, test_data_path, test_labels_path)
+
+    tiny_dataset_path = 'tinydataset/original_data.csv'
+    tiny_train_path = 'tinydataset/train.csv'
+    tiny_test_path = 'tinydataset/test.csv'
+    tiny_header_path = 'tinydataset/header.csv'
+    tiny_train_data_path = 'tinydataset/train_data.csv'
+    tiny_train_labels_path = 'tinydataset/train_labels.csv'
+    tiny_test_data_path = 'tinydataset/test_data.csv'
+    tiny_test_labels_path = 'tinydataset/test_labels.csv'
+    tiny_size = 100
+    tiny_split = 70
+
+    make_tiny_dataset(orig_path, tiny_dataset_path, tiny_size)
+    split_dataset(tiny_dataset_path, tiny_train_path, tiny_test_path, tiny_header_path, tiny_split)
+    split_labels(tiny_train_path, tiny_train_data_path, tiny_train_labels_path)
+    split_labels(tiny_test_path, tiny_test_data_path, tiny_test_labels_path)
+
+if __name__ == "__main__":
+    main()
